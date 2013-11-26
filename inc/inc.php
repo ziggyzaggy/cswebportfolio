0b8f0c1ca42493cacd0bf74aeeffafec67762373
Kristjan <meres87@gmail.com>
2013-11-26 14:36:48 +0000

    Now uses PDO

--- inc/inc.php
+++ inc/inc.php
@@ -1,7 +1,23 @@
 
 <?php
-@mysql_connect("localhost","root","") or die("failed to connect to database");
+/*mysql_connect("localhost","root","") or die("failed to connect to database");
+*/
 
-mysql_select_db("csdm");
+if(class_exists('PDO'))
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$db = "csdm";
+
+try{
+	$dbh = new PDO("mysql:host=$hostname; dbname=$db", $username, $password);
+	echo "connected to database";
+}
+	catch(PDOException $exc)
+{
+	echo $exc -> getMessage();
+}
+
+//mysql_select_db("csdm");
 
 ?>
