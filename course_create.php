7de23a1e27ed9ec5de4043fe923479f8a17b5662
0909007 <0909007@RG41772.rgu.ac.uk>
2013-11-28 10:55:23 +0000

    tidied up course_create,read,update

--- course_create.php
+++ course_create.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
+<!--author: eduard tache (0909007)-->
 
+<?php require_once "inc/inc.php"; ?>
 
-<html>
-
-<head>
-<title></title>
-<meta charset=?tf-8?
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/bootstrap-responsive.css">
-	<link rel="stylesheet" href="css/style.css">
-	
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="js/bootstrap.js"></script>
-</head>
+<?php include 'header.php'; ?>
 
 <body>
 	<div class = "container">
-		<div class = "navbar navbar-static-top" style ="margin-bottom:20px;">
-			<div class ="navbar-inner">
-                                <a href="index.php" class="brand">LOGO</a>
-                                <ul class = "nav">
-                                    <li> <a href="index.php">Home</a></li>
-                                    <li> <a href="reading.php">Reading List</a></li> 
-                                </ul>
-                                <ul class = "nav pull-right">
-                                    <li> <a href="admin.php">Admin</a></li>
-                                </ul>
-			</div>	
-		</div>
+            <?php include 'navigation_bar.php' ?>
             <h1>Create Course</h1>
                 <form class="well">  
                     <label>Course Name</label>  
@@ -44,4 +21,4 @@
         </div>
 	
 </body>
-</html>
+<?php include 'footer.php' ?>
