e13d91e0b5082021f422cbad569f0530488feaae
Kristjan <meres87@gmail.com>
2013-11-28 10:51:46 +0000

    results now have links that lead to a page displaying the details of a book

---
+++ display.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+<title></title>
+<meta charset=?tf-8?
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/style.css">
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+
+		
+
+	
+	
+
+</head>
+				<?php
+						
+						include_once('display.backbone.php');
+						
+				?>		
+
+<body>
+
+
+
+
+
+
+	<div class = "container">
+		<div class = "navbar navbar-static-top" style ="margin-bottom:20px;">
+			<div class ="navbar-inner">
+				
+					<a href="index.php" class="brand">LOGO</a>
+				
+					<ul class = "nav">
+					<li> <a href="index.php">Home</a></li>
+					<li class="active"> <a href="reading.php">Reading List</a></li> 
+					</ul>
+					
+					<ul class = "nav pull-right">
+					<li> <a href="admin.php">Admin</a></li>
+					
+					</ul>
+					
+				
+			</div>	
+		</div>		
+			
+	
+	
+	
+		
+			
+			
+			<div class="row-fluid">
+			
+				<div class="span2 ">
+					<ul class="nav nav-list well">
+						<li class="nav-header">Menu</li>
+						<li><a href="reading.php">Browse</a></li>
+						<li><a href="search.php">Search</a></li>
+						
+					
+					</ul>
+				
+				</div>
+			
+				<div class ="span10 well">
+				
+					<div class="row fluid">
+					
+						<div class="span12" style="margin-left:10px;">
+							<h3 style="margin-left:30px;"><?php echo $bookTitle;?></h3>
+							<ul class="nav nav-list">
+								<li class="divider"></li>
+							</ul>
+						
+						</div>
+					
+					
+					</div>
+					
+					
+					
+					
+					<div class="well">
+				
+					<?php
+	
+						
+							 echo $bookTitle;  
+							 echo $noBookSelected;
+
+						?>
+					</div>
+				</div>
+				
+				
+					
+				
+			
+			</div>
+		
+	</div>	
+	
+</body>
+</html>	
---
+++ reading.backbone.php
@@ -0,0 +1,60 @@
+<?php
+
+
+include('inc/inc.php');
+
+	if(isset($_GET['category'])){
+	
+	
+	
+	$i = 1;
+	
+	
+		if($_GET['category'] == 'Module'){
+		
+		
+		
+		 echo "<h4>Module books: </h4> <br>";
+		 
+			$result = $dbh -> query("SELECT username FROM admin");
+		 
+		 foreach($result as $row){
+			echo "<a href = display.php?id=" . $row['username'] . ">". $i. "&nbsp &nbsp View <b> " . $row['username'] . "</b> </a>  <br>";
+			$i++;
+		 }
+		
+		}elseif($_GET['category'] == 'Course and Year'){
+		
+		 echo "<h4>Course and Year books: </h4> <br>";
+		 
+		 $result = $dbh -> query("SELECT password FROM admin");
+		 
+		 foreach($result as $row){
+			echo "<a href = display.php?id=" . $row['password'] . ">". $i. "&nbsp &nbsp View <b> " . $row['password'] . "</b> </a>  <br>";
+			$i++;
+		 }
+		 
+		
+		}if($_GET['category'] == 'Course'){
+		
+		  echo "<h4>Course books:</h4> <br>";
+		  
+		  
+		  $result = $dbh -> query("SELECT username FROM admin");
+		 
+		  foreach($result as $row){
+			echo "<a href = display.php?id=" . $row['username'] . ">". $i. "&nbsp &nbsp View <b> " . $row['username'] . "</b> </a>  <br>";
+			$i++;
+		 }
+		
+		}
+	
+	
+	}else{
+		echo "Don't be shy, <br> Click one of the categories above to browse books!";
+	
+	}
+
+
+
+?>
