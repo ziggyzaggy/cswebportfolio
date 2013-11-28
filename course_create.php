77bfc43a24a7f87f58e7c0225aac0ffd14c2cbfc
0909007 <0909007@RG42826.rgu.ac.uk>
2013-11-28 16:05:53 +0000

    work in progress - course create needs to save to db

--- course_create.php
+++ course_create.php
@@ -5,19 +5,32 @@
 <?php include 'header.php'; ?>
 
 <body>
+    
 	<div class = "container">
             <?php include 'navigation_bar.php' ?>
             <h1>Create Course</h1>
-                <form class="well">  
-                    <label>Course Name</label>  
-                    <input type="text" class="span3">  
-                    <label>Year of Entry</label>  
-                    <input type="number" class="span3" min="0" max="5">
-                    <label>Course Duration</label>  
-                    <input type="number" class="span3" min="0" max="5">
-                    <br>
-                    <button type="submit" class="btn">Submit</button>  
-                </form>
+            <?php
+                if(isset($_POST['create'])){
+                    $cname = $_POST['course_name'];
+                    $yentry = $_POST['year_of_entry'];
+                    $duration = $_POST['duration'];
+                    echo "Course name: ".$cname;
+                    echo "year of entry: ".$yentry;
+                    echo "duration: ".$duration;
+                }
+            ?>
+            <form class="well" action="" method="POST">  
+                <label>Course Name</label>  
+                <input name="course_name" type="text" class="span3">  
+                <label>Year of Entry</label>  
+                <input name="year_of_entry" type="number" class="span3" min="0" max="5">
+                <label>Course Duration</label>  
+                <input name="duration" type="number" class="span3" min="0" max="5">
+                <br>
+                <button name="create" type="submit" class="btn">Submit</button>  
+            </form>
+            
+            
         </div>
 	
 </body>
