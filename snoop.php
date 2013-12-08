71b5486bd8022d57859620cb5dcefef42d1150a6
Kristjan <meres87@gmail.com>
2013-12-08 23:12:45 +0000

    backend rewritten to work with

--- snoop.php
+++ snoop.php
@@ -5,58 +5,66 @@ include_once('inc/inc.php');
 /**
 **
 ** @ Author Kristjan Muutnik 1308701
-**	The backbone of search returns the results that match a keyword
+**      The backbone of search returns the results that match a keyword
 */
 
-if(isset($_GET['snoop'])){		//check that the button was clicked
+if(isset($_GET['snoop'])){              //check that the button was clicked
 
-$search = $_GET['searchField'];		//pass the get variable in to a variable
+$search = $_GET['searchField'];         //pass the get variable in to a variable
 
-if(!empty($_GET['searchField'])){		//check that the search field wasn't empty
-	
-	$result = $dbh->query				//query the database based on user input
-	
-				("
-	
-				SELECT *
-				FROM books
-				WHERE Content_Summary LIKE '%$search%'
-				
-				"
-				);
-				
-				
-		$rowCounter = $result->rowCount($result);	//count amount of returned records
-				
-		if($rowCounter > 0){			//if more than 0 records were returned		
-			echo"<div class='well'>";
-				
-				
-			($rowCounter == 1 ? $humanise = "item" : $humanise = "items"); 	
-				
-			
-				echo"
-				 <b>$rowCounter</b> $humanise found: <br> <br>";
-		
-				
-				$i = 1;
-				
-				foreach($result as $row){ 		//loops through the results echoing them out
-					
-					
-					echo "<a href = display.php?id=" . $row['Book_ID'] . ">". $i. "&nbsp &nbsp View <b> " . $row['Title'] . "</b> </a>  <br>";
-					$i++;
-					}
+if(!empty($_GET['searchField'])){               //check that the search field wasn't empty
+        
+        $result = $conn->query                           //query the database based on user input
+        
+                                ("
+        
+                                SELECT *
+                                FROM books
+                                WHERE Content_Summary LIKE '%$search%'
+                                
+                                "
+                                );
+                                
+                                
+                $rowCounter = $result->rowCount($result);       //count amount of returned records
+                                
+                if($rowCounter > 0){                    //if more than 0 records were returned          
+                        echo"<div class='well'>";
+                                
+                                
+                        ($rowCounter == 1 ? $humanise = "item" : $humanise = "items");  
+                                
+                        
+                                echo"
+                                 <b>$rowCounter</b> $humanise found: <br> <br>";
+                
+                                
+                                $i = 1;
+                                
+                                foreach($result as $row){               //loops through the results echoing them out
+                                        
+                                        
+                                        echo "<a href = display.php?id=" . $row['Book_ID'] . ">". $i. "&nbsp &nbsp View <b> " . $row['Title'] . "</b> </a>  <br>";
+                                        $i++;
+                                }
 
-		
-		}else{				//if no records were returned
-			echo "Nope, nothing";
-		}
+                
+                }else{                          //if no records were returned
+                        echo "Nope, nothing";
+                }
 
 }
 echo "</div>";
-}	
+}       
 
 // end of Kristjan Muutnik 1308701
 
 ?>
+<html>
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <title></title>
+  </head>
+  <body></body>
+</html>
